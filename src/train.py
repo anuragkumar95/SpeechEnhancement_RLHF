dc8d71a51a0198e0b68fefd8c9b07454d5647594
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-10-04 21:31:18 -0400

    debug

--- src/train.py
+++ src/train.py
@@ -229,7 +229,7 @@ class DDPGTrainer:
             actor_loss = -self.critic(experience['curr']['clean_mag'], a_next_state['est_mag']).mean()
             """
             actor_loss = -self.critic(env.state['clean_mag'], next_state['clean_mag']).mean()
-            critic_loss = -reward
+            critic_loss = reward
             #Update networks
             self.c_optimizer.zero_grad()
             critic_loss.backward()

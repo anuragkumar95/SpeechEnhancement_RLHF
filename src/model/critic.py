3c938b9954a32040889ad7944deb070e30c8784f
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-10-27 10:11:59 -0400

    .

--- src/model/critic.py
+++ src/model/critic.py
@@ -4,6 +4,7 @@ from pesq import pesq
 import torch
 import torch.nn as nn
 from utils import LearnableSigmoid, power_uncompress
+from speech_enh_env import SpeechEnhancementAgent
 
 
 class QNet(nn.Module):
@@ -39,8 +40,16 @@ class QNet(nn.Module):
             LearnableSigmoid(1),
         )
         self.gpu_id = gpu_id
+        self.agent = SpeechEnhancementAgent(window=25, 
+                                            buffer_size=1000,
+                                            n_fft=400,
+                                            hop=100,
+                                            gpu_id=self.gpu_id,
+                                            args=None)
 
     def forward(self, x, y, t):
+        next_state = self.agent.get_next_state(x, y, t)
+        """
         x_real = x['est_real']
         x_imag = x['est_imag']
         mag = x['est_mag']
@@ -48,8 +57,6 @@ class QNet(nn.Module):
         complex_out = y[1]
         
         noisy_phase = torch.angle(torch.complex(x_real, x_imag))
-        print(f"x_real:{x_real.shape}, x_imag:{x_imag.shape}")
-        print(f"Mag:{mag.shape}, mask:{mask.shape}, compl:{complex_out.shape}, phase:{noisy_phase.shape}")
         out_mag = mask * mag[:, :, :, t].unsqueeze(-1)
 
         mag_real = out_mag * torch.cos(noisy_phase[:, :, :, t].unsqueeze(-1))#.permute(0, 1, 3, 2)
@@ -57,15 +64,15 @@ class QNet(nn.Module):
 
         final_real = mag_real + complex_out[:, 0, :, :].unsqueeze(1)
         final_imag = mag_imag + complex_out[:, 1, :, :].unsqueeze(1)
-        print(f"final_real:{final_real.shape}")
         est_spec_uncompress = power_uncompress(final_real, final_imag).squeeze(1).permute(0,3,1,2)
 
-        print(f"uncompressed:{est_spec_uncompress.shape}")
         final_mag = torch.sqrt(est_spec_uncompress[:, 0, :, :]**2 + est_spec_uncompress[:, 1, :, :]**2)
-        print(f"Final:{final_mag.shape}")
         final_mag = final_mag.squeeze(-1).unsqueeze(1)
         mag[:, :, :, t] = final_mag
         clean_mag = x['clean_mag']
-        
+        """
+        mag = next_state['est_mag']
+        clean_mag = next_state['clean_mag']
+
         xy = torch.cat([mag, clean_mag], dim = 1)
         return self.layers(xy)

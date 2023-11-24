3d80aafa83ef1ac6a6293abe36c6da65ba9f930e
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-24 13:10:35 -0500

    debug

--- src/reward_model/src/models/reward_model.py
+++ src/reward_model/src/models/reward_model.py
@@ -82,12 +82,13 @@ class RewardModel(nn.Module):
         )
 
         self.softmax = nn.Softmax(dim=-1)
+        self.batch_norm = nn.BatchNorm2D(1)
 
     def forward(self, wav_in, wav_out):
         
         #xy = torch.cat([wav_in, wav_out], dim = 1)
         #xy = wav_in - wav_out
-        dist = self.layers(wav_in) - self.layers(wav_out)
+        dist = self.batch_norm(self.layers(wav_in) - self.layers(wav_out))
         print(f"dist:{dist.mean()}")
         logits = self.classification_head(dist)
         probs = self.softmax(logits)

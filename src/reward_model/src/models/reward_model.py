dcfcddfba66482892d176d44e9c2aa19a710ba87
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-27 17:10:05 -0500

    .

--- src/reward_model/src/models/reward_model.py
+++ src/reward_model/src/models/reward_model.py
@@ -93,3 +93,173 @@ class RewardModel(nn.Module):
 
         return probs
     
+class LossNet(nn.Module):
+    def __init__(self, in_channels, n_layers, kernel_size, keep_prob, norm_type='sbn'):
+        super().__init__()
+        self.net = nn.ModuleList()
+        self.n_layers = n_layers
+        for i in range(n_layers):
+            out_channels = 32 * (2 ** (i // 5))
+            prev_out = 32 * (2 ** ((i-1) // 5))
+            if i == 0:
+                if norm_type == 'sbn':
+                    layer = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nn.BatchNorm2d(out_channels),
+                        nn.LeakyReLU(0.2),
+                        nn.Dropout(1 - keep_prob),
+                    )
+                if norm_type == 'nm':
+                    layer = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nm_torch(out_channels),
+                        nn.LeakyReLU(0.2),
+                        nn.Dropout(1 - keep_prob)
+                    )
+                if norm_type == 'none':
+                    layer = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nn.Dropout(1 - keep_prob),
+                        nn.LeakyReLU(0.2),
+                    )
+
+            elif i == n_layers - 1:
+                if norm_type == 'sbn':
+                    layer = nn.Sequential(
+                        nn.Conv2d(prev_out, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nn.BatchNorm2d(out_channels),
+                        nn.LeakyReLU(0.2),
+                    )
+                if norm_type == 'nm':
+                    layer = nn.Sequential(
+                        nn.Conv2d(prev_out, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nm_torch(out_channels),
+                        nn.LeakyReLU(0.2),   
+                    )
+                if norm_type == 'none':
+                    layer = nn.Sequential(
+                        nn.Conv2d(prev_out, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nn.LeakyReLU(0.2),
+                    )
+            else:
+                if norm_type == 'sbn':
+                    layer = nn.Sequential(
+                        nn.Conv2d(prev_out, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nn.BatchNorm2d(out_channels),
+                        nn.LeakyReLU(0.2),
+                        nn.Dropout(1 - keep_prob),
+                    )
+                if norm_type == 'nm':
+                    layer = nn.Sequential(
+                        nn.Conv2d(prev_out, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nm_torch(out_channels),
+                        nn.LeakyReLU(0.2),
+                        nn.Dropout(1 - keep_prob)
+                    )
+                if norm_type == 'none':
+                    layer = nn.Sequential(
+                        nn.Conv2d(prev_out, out_channels, kernel_size, 2, padding=1),
+                        #nn.ZeroPad2d((0, 0, 0, 1)),
+                        nn.LeakyReLU(0.2),
+                        nn.Dropout(1 - keep_prob)
+                    )
+            self.net.append(layer)
+        
+    def forward(self, x):
+        outs = []
+        for i, layer in enumerate(self.net):
+            x = layer(x)
+            outs.append(x)
+        return outs
+
+class ClassificationHead(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+        self.dense3 = nn.Linear(in_dim, 16)
+        self.dense4 = nn.Linear(16, 6)
+        self.dense2 = nn.Linear(6, out_dim)
+        self.relu = nn.LeakyReLU(0.2)
+        if out_dim == 1:
+            self.sigmoid = nn.Sigmoid()
+        else:
+            self.softmax = nn.Softmax(dim = -1)
+        self.outputs = out_dim
+
+    def forward(self, x):
+        out = self.relu(self.dense3(x))
+        out = self.relu(self.dense4(out))
+        out = self.dense2(out)
+        if self.outputs == 1:
+            scores = self.sigmoid(out)
+        if self.outputs == 2:
+            scores = self.softmax(out)
+        return scores
+
+                
+class FeatureLossBatch(nn.Module):
+    def __init__(self, n_layers, base_channels, sum_till=14, weights=False, gpu_id=None):
+        super().__init__()
+        self.out_channels = [base_channels * (2 ** (i // 5)) for i in range(n_layers)]
+        self.sum_last_layers=sum_till
+        self.n_layers = n_layers
+        if weights:
+            self.weights = [nn.Parameter(torch.randn(features), requires_grad=True) for features in self.out_channels]
+            if gpu_id is not None:
+                self.weights = [param.to(gpu_id) for param in self.weights]
+        else:
+            self.weights = None
+
+    def forward(self, embeds1, embeds2):
+        """
+        Both embeds1 and embeeds are outputs from each layer of
+        loss_net. 
+        """
+        loss_final = 0
+        for i, (e1, e2) in enumerate(zip(embeds1, embeds2)):
+            if i >= self.n_layers - self.sum_last_layers:
+                dist = e1 - e2
+                dist = dist.permute(0, 3, 2, 1)
+                if self.weights is not None:
+                    res = (self.weights[i] * dist)
+                else:
+                    res = dist
+                loss = torch.mean(res, dim=[2, 3])
+                loss_final += loss
+        return loss_final
+
+
+class JNDModel(nn.Module):
+    def __init__(self, in_channels, n_layers=14, keep_prob=0.7, norm_type='sbn', sum_till=14, gpu_id=None):
+        super().__init__()
+        self.loss_net = LossNet(in_channels=in_channels, 
+                                n_layers=n_layers, 
+                                kernel_size=3, 
+                                keep_prob=keep_prob, 
+                                norm_type=norm_type)
+
+        self.classification_layer = ClassificationHead(in_dim=1, out_dim=2)
+
+        self.feature_loss = FeatureLossBatch(n_layers=n_layers,
+                                             base_channels=32,
+                                             gpu_id=gpu_id,
+                                             weights=True,
+                                             sum_till=sum_till)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, inp, ref):
+        ref = self.loss_net(ref)
+        inp = self.loss_net(inp)
+        dist = self.feature_loss(ref, inp)
+        dist = self.sigmoid(dist).reshape(-1, 1)
+        logits = self.classification_layer(dist)
+        
+        return logits
+    

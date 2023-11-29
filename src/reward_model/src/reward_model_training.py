f8f7367ed24c9bc123e7f95646611de0a9dd3e6b
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2023-11-29 17:10:07 -0500

    .

--- src/reward_model/src/reward_model_training.py
+++ src/reward_model/src/reward_model_training.py
@@ -267,11 +267,11 @@ class Trainer:
                 self.save(save_path, _dict_)
 
             #Run validation after loading checkpoint
-            print(f"save_path:{save_path}")
-            if save_path is not None:
-                eval = Evaluation(save_path, self.gpu_id)
-                preds, labels = eval.predict(self.test_ds)
-                eval.score(preds, labels)
+            #print(f"save_path:{save_path}")
+            #if save_path is not None:
+            #    eval = Evaluation(save_path, self.gpu_id)
+            #    preds, labels = eval.predict(self.test_ds)
+            #    eval.score(preds, labels)
                               
 def main(args):
     if args.parallel:

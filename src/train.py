5d3a3091b5ac35df9cfe5c2cd70723bfae9d924e
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-01-26 16:38:24 -0500

    .

--- src/train.py
+++ src/train.py
@@ -446,7 +446,7 @@ def main(rank: int, world_size: int, args):
 
 
 if __name__ == "__main__":
-    ARGS = args()
+    ARGS = args
 
     output = f"{ARGS.save_model_dir}/{ARGS.exp}"
     os.makedirs(output, exist_ok=True)

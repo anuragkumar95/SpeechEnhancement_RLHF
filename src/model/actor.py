ee8e1ff0ba2ccdf2a68aafa321d3cfdd9551e465
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-02-18 22:58:20 -0500

    .

--- src/model/actor.py
+++ src/model/actor.py
@@ -276,15 +276,15 @@ class RewardModel(nn.Module):
     def __init__(self, policy):
         super(RewardModel, self).__init__()
         self.conformer = policy
-        self.reward_projection = QNet(ndf=16, out_channel=2)
+        self.reward_projection = QNet(ndf=16, in_channel=128, out_channel=2)
         
     def forward(self, x_ref, x_per):
 
         x_ref = x_ref.permute(0, 1, 3, 2)
         x_per = x_per.permute(0, 1, 3, 2)
 
-        ref_emb = self.conformer.get_embedding(x_ref).permute(0, 2, 3, 1)
-        per_emb = self.conformer.get_embedding(x_per).permute(0, 2, 3, 1)
+        ref_emb = self.conformer.get_embedding(x_ref)
+        per_emb = self.conformer.get_embedding(x_per)
 
         print(f"ref:{ref_emb.shape}, per:{per_emb.shape}")
         

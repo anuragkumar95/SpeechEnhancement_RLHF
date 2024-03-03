26b366f9d56470e2f4a2255799cccc3f9299623b
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-02 21:25:11 -0500

    .

--- src/speech_enh_env.py
+++ src/speech_enh_env.py
@@ -129,7 +129,8 @@ class SpeechEnhancementAgent:
             pesq_reward = pesq_reward.to(self.gpu_id)
             exp_pesq_reward = exp_pesq_reward.to(self.gpu_id)
 
-        return pesq_reward.mean() - exp_pesq_reward.mean()
+        return pesq_reward.mean()
+        #return pesq_reward.mean() - exp_pesq_reward.mean()
     
 
 class replay_buffer:

2c8da10d769812973cf69a706a0a1bf05d7ca753
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-08 21:01:58 -0500

    .

--- src/train.py
+++ src/train.py
@@ -153,17 +153,33 @@ class Trainer:
 
         if self.dist == "Categorical":
             est_reals, est_imags, comp_real_probs, comp_imag_probs = self.model(noisy_spec)
-            dist_reals = (est_reals - clean_real) ** 2
-            dist_imags = (est_imags - clean_real) ** 2
+            dist_reals = []
+            dist_imags = []
+            for i in range(est_reals.shape[-1]):
+                dist_real = (est_reals[..., i] - clean_real) ** 2
+                dist_imag = (est_imags[..., i] - clean_imag) ** 2
+                dist_reals.append(dist_real)
+                dist_imags.append(dist_imag)
+
+            dist_reals = torch.stack(dist_reals, dim=-1)
+            dist_imags = torch.stack(dist_imags, dim=-1)
+
+            print(f"dist:{dist_reals.shape}")
 
             target_k_real = torch.argmin(dist_reals, dim=-1)
             target_k_imag = torch.argmin(dist_imags, dim=-1)
 
+            print(f"tgt_k:{target_k_real.shape}")
+
             pred_k_real = torch.argmax(comp_real_probs, dim=-1)
             pred_k_imag = torch.argmax(comp_imag_probs, dim=-1)
 
+            print(f"pred_k:{pred_k_real.shape}")
+
             est_real = torch.gather(est_reals, -1, pred_k_real)
             est_imag = torch.gather(est_imags, -1, pred_k_imag)
+
+            print(f"est_real:{est_real.shape}")
         else:
             est_real, est_imag = self.model(noisy_spec)
         

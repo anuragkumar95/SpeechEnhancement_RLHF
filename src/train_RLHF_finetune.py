86c5545f4e2f4ba0a235c0daf7c7e56efb9b4c76
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-16 15:44:57 -0400

    .

--- src/train_RLHF_finetune.py
+++ src/train_RLHF_finetune.py
@@ -162,7 +162,7 @@ class Trainer:
             self.trainer = PPO(init_model=self.expert, 
                                reward_model=self.reward_model, 
                                gpu_id=gpu_id, 
-                               beta=1e-06,
+                               beta=1e-01,
                                val_coef=0.8,
                                en_coef=0,
                                discount=0.98,

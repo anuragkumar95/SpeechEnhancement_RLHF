9d5c80c1077fc7b871e3139e5c606d68d4538378
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-17 01:39:47 -0400

    .

--- src/RLHF.py
+++ src/RLHF.py
@@ -572,9 +572,9 @@ class PPO:
                 G = self.env.get_PESQ_reward(next_state)
             else:
                 r_t = self.env.get_RLHF_reward(inp=states[t], out=next_state['noisy'].permute(0, 1, 3, 2))
-                self._r_mean = (self.t * self._r_mean + r_t.mean().detach()) / (self.t + 1)
-                self._r2_mean = (self.t * self._r_mean + (r_t**2).mean()) / (self.t + 1)
-                G = self.scale_reward(r_t) - self.beta * torch.abs(kl_penalty)
+                #self._r_mean = (self.t * self._r_mean + r_t.mean().detach()) / (self.t + 1)
+                #self._r2_mean = (self.t * self._r_mean + (r_t**2).mean()) / (self.t + 1)
+                G = r_t - self.beta * kl_penalty
 
             optimizer.zero_grad()
             clip_loss.backward()

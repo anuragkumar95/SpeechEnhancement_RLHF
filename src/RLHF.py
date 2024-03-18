2cf0a904659782c9eab68f41dc413bc1f7b19a35
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-18 12:46:44 -0400

    .

--- src/RLHF.py
+++ src/RLHF.py
@@ -358,14 +358,17 @@ class PPO:
             #Get new logprobs and values for the sampled (state, action) pair
             mb_action = ([actions[i]['action'][0] for i in mb_indx],
                          [actions[i]['action'][1] for i in mb_indx])
-            mb_action = (torch.stack(mb_action[0]).unsqueeze(1), torch.stack(mb_action[1]))
+            mb_action = (torch.stack(mb_action[0]).squeeze(1), torch.stack(mb_action[1]))
             print(f"mb_action:{mb_action[0].shape, mb_action[1].shape}")
             mb_params = (([actions[i]['params'][0][0] for i in mb_indx], [actions[i]['params'][0][1] for i in mb_indx]), 
                          ([actions[i]['params'][1][0] for i in mb_indx], [actions[i]['params'][1][1] for i in mb_indx]))
             mb_params = ((torch.stack(mb_params[0][0]), torch.stack(mb_params[0][1])), 
                          (torch.stack(mb_params[1][0]), torch.stack(mb_params[1][0])))
             print(f"mb_params:{mb_params[0][0].shape, mb_params[0][1].shape, mb_params[1][0].shape, mb_params[1][1].shape}")
-            log_probs, entropies = actor.get_action_prob(action, params)
+            log_probs, entropies = actor.get_action_prob(mb_action, mb_params)
+
+            mb_states = torch.stack(states[mb_indx])
+            print(f"mb_states:{mb_states.shape}")
             values = critic(states[t])
             VALUES[:, t] = values.reshape(-1)
                 

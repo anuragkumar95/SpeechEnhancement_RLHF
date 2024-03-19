a21e8fbb4e686a14e024af4247f398a52794663e
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-18 22:55:12 -0400

    .

--- src/model/actor.py
+++ src/model/actor.py
@@ -292,7 +292,7 @@ class TSCNet(nn.Module):
         #out_4 = self.TSCB_3(out_3)
         #out_5 = self.TSCB_4(out_4)
        
-        _, m_logprob, m_entropy, _ = self.mask_decoder(out_2, action[0].permute(0, 1, 3, 2))
+        _, m_logprob, m_entropy, _ = self.mask_decoder(out_2, action[0].squeeze(1).permute(0, 2, 1))
         _, c_logprob, c_entropy, _ = self.complex_decoder(out_2, action[1])
         return (m_logprob, c_logprob), (m_entropy, c_entropy)
         

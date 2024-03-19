09e5e960865f26196923cb0b260a5a8c6318eb20
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-19 19:15:41 -0400

    .

--- src/model/actor.py
+++ src/model/actor.py
@@ -145,9 +145,6 @@ class MaskDecoder(nn.Module):
     def sample(self, mu, x=None):
         #sigma = torch.abs(torch.exp(0.5 * logvar) + 1e-08)
         sigma = torch.ones(mu.shape).to(self.gpu_id)
-        #print(f"mu:{mu.mean()}, sigma:{sigma.mean()}")
-        #if x is not None:
-        #    print(f"x:{x.shape, x.mean()}")
         N = Normal(mu, sigma)
         if x is None:
             x = N.rsample()

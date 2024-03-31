286107a7f14878416e4ba398a591242b74e5549a
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-03-31 11:12:06 -0400

    .

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -242,6 +242,10 @@ class EvalModel:
         est_audio = est_audio / c
         est_audio = torch.flatten(est_audio)[:length].cpu().numpy()
 
+        if len(est_audio) < length:
+            pad = np.zeros(length - len(est_audio))
+            est_audio = np.stack([est_audio, pad], dim=-1)
+
         assert len(est_audio) == length, f"est:{len(est_audio)}, inp:{length}"
         saved_path = os.path.join(saved_dir, name)
         sf.write(saved_path, est_audio, sr)

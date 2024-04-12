44c69af5eb204447a80ad705a8ddd6ccb23b79f4
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-12 19:29:33 -0400

    .

--- src/RLHF.py
+++ src/RLHF.py
@@ -295,7 +295,7 @@ class PPO:
     def unroll_policy(self, actor, critic):
         #Set models to eval
         actor = actor.eval()
-        actor.set_evaluation(False)
+        actor.set_evaluation(True)
         critic = critic.eval()
 
         rewards = []

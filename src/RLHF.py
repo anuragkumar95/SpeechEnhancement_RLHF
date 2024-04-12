2c570c7d71e7fc8db22148cd89ede5ea5c52f405
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-12 14:42:56 -0400

    .

--- src/RLHF.py
+++ src/RLHF.py
@@ -325,6 +325,10 @@ class PPO:
                 bs, ch, t, f = clean.shape
 
                 action, log_probs, _, _ = actor.get_action(noisy)
+
+                action_prob, _ = actor.get_action_prob(noisy, action)
+
+                print(f"action: 1: {log_probs.mean(), 2:{action_prob.mean()}}")
                 
                 if self.init_model is not None:
                     init_action, _, _, _ = self.init_model.get_action(noisy)

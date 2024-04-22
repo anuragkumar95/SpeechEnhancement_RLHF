cef911f62e56af0b0c83988de359cbbe60fcaa7f
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-22 01:34:04 -0400

    .

--- src/evaluation.py
+++ src/evaluation.py
@@ -9,6 +9,7 @@ import soundfile as sf
 import argparse
 from tqdm import tqdm
 import pickle
+import traceback
 
 
 @torch.no_grad()
@@ -25,6 +26,8 @@ def enhance_one_track(
     noisy = torch.transpose(noisy * c, 0, 1)
 
     length = noisy.size(-1)
+    if length > cut_len:
+        return None, None
     frame_num = int(np.ceil(length / 100))
     padded_len = frame_num * 100
     padding_len = padded_len - length
@@ -109,13 +112,14 @@ def evaluation(model_path, noisy_dir, clean_dir, cutlen, save_tracks, saved_dir,
                 est_audio, length = enhance_one_track(
                     model, noisy_path, saved_dir, cutlen, n_fft, n_fft // 4, save_tracks
                 )
-                clean_audio, sr = sf.read(clean_path)
-                assert sr == 16000
-                metrics = compute_metrics(clean_audio, est_audio, sr, 0)
-                metrics = np.array(metrics)
-                metrics_total += metrics
-                results['pesq'].append(metrics[0])
-                results['file'].append(audio)
+                if est_audio is not None:
+                    clean_audio, sr = sf.read(clean_path)
+                    assert sr == 16000
+                    metrics = compute_metrics(clean_audio, est_audio, sr, 0)
+                    metrics = np.array(metrics)
+                    metrics_total += metrics
+                    results['pesq'].append(metrics[0])
+                    results['file'].append(audio)
         except Exception as e:
             import traceback
             print(traceback.format_exc())
@@ -141,6 +145,157 @@ def evaluation(model_path, noisy_dir, clean_dir, cutlen, save_tracks, saved_dir,
 
     with open(os.path.join(saved_dir, 'results.pickle'), 'wb') as f:
         pickle.dump(results, f)
+
+'''
+def run_validation_step(self, env, clean, noisy):
+        """
+        Runs a vlidation loop for a batch.
+        Predict mask for each frame one at a time 
+        and return pesq score of the enhances batch of 
+        spectrograms.
+        """
+        metrics = {
+            'pesq':[],
+            'csig':[],
+            'cbak':[],
+            'covl':[],
+            'ssnr':[],
+            'stoi':[],
+            'si-sdr':[],
+            'mse':0,
+            'reward':0}
+        #print("Running validation...")
+
+        clean_aud, clean, noisy, _ = batch
+        inp = noisy.permute(0, 1, 3, 2)
+
+        #Forward pass through actor to get the action(mask)
+        action, _, _, _ = self.actor.get_action(inp)
+        if self.expert is not None:
+            exp_action, _, _, _ = self.expert.get_action(inp)
+
+        if self.args.train_phase:
+            a_t = action
+        else:
+            a_t = (action[0], exp_action[-1])
+        
+        #Apply action  to get the next state
+        next_state = env.get_next_state(state=inp, 
+                                        action=a_t)
+        
+        #Get reward
+        r_state = self.trainer.env.get_RLHF_reward(state=next_state['noisy'].permute(0, 1, 3, 2), 
+                                       scale=False).sum()
+        metrics['reward'] = r_state
+
+        #Supervised loss
+        mb_enhanced = next_state['noisy'].permute(0, 1, 3, 2)
+        mb_enhanced_mag = torch.sqrt(mb_enhanced[:, 0, :, :]**2 + mb_enhanced[:, 1, :, :]**2)
+        
+        mb_clean_mag = torch.sqrt(clean[:, 0, :, :]**2 + clean[:, 1, :, :]**2)
+
+        supervised_loss = ((clean - mb_enhanced) ** 2).mean() + ((mb_clean_mag - mb_enhanced_mag)**2).mean()
+        metrics['mse'] = supervised_loss
+
+        #Calculate metrics
+        #pesq, pesq_mask = batch_pesq(clean_aud.detach().cpu().numpy(), 
+        #                             next_state['est_audio'].detach().cpu().numpy())
+        
+        for i in range(self.args.batchsize):
+            values = compute_metrics(clean_aud[i, ...].detach().cpu().numpy(), 
+                                     next_state['est_audio'][i, ...].detach().cpu().numpy(), 
+                                     16000, 
+                                     0)
+            metrics['pesq'].append(values[0])
+            metrics['csig'].append(values[1])
+            metrics['cbak'].append(values[2])
+            metrics['covl'].append(values[3])
+            metrics['ssnr'].append(values[4])
+            metrics['stoi'].append(values[5])
+            metrics['si-sdr'].append(values[6])
+        
+        #Calculate NISQA mos
+        """
+        ds = NL.SpeechQualityDataset(df=next_state['est_audio'].detach().cpu().numpy(), data_dir=None)
+
+        val_mos, _ = NL.predict_mos(self.nisqa.model, 
+                                    ds = ds, 
+                                    bs=self.args.batchsize, 
+                                    dev=self.gpu_id, 
+                                    num_workers=0)
+        """
+        return metrics
+    
+
+
+    def run_validation(self, episode):
+        #Run validation
+        self.actor.eval()
+        if self.args.method == 'PPO':
+            self.actor.set_evaluation(True)
+            self.critic.eval()
+        pesq = 0
+        loss = 0
+        val_metrics = {
+            'pesq':[],
+            'csig':[],
+            'cbak':[],
+            'covl':[],
+            'ssnr':[],
+            'stoi':[],
+            'si-sdr':[],
+            'reward':0,
+            'mse':0
+        }
+        num_batches = len(self.test_ds)
+        with torch.no_grad():
+            for i, batch in enumerate(self.test_ds):
+                
+                #Preprocess batch
+                batch = preprocess_batch(batch, gpu_id=self.gpu_id)
+                
+                #Run validation episode
+                try:
+                    metrics = self.run_validation_step(self.trainer.env, batch)
+                    val_metrics['pesq'].extend(metrics['pesq'])
+                    val_metrics['csig'].extend(metrics['csig'])
+                    val_metrics['cbak'].extend(metrics['cbak'])
+                    val_metrics['covl'].extend(metrics['covl'])
+                    val_metrics['ssnr'].extend(metrics['ssnr'])
+                    val_metrics['stoi'].extend(metrics['stoi'])
+                    val_metrics['si-sdr'].extend(metrics['si-sdr'])
+                    val_metrics['mse'] += metrics['mse']
+                    val_metrics['reward'] += metrics['reward']
+
+                except Exception as e:
+                    print(traceback.format_exc())
+                    continue
+        
+        loss = val_metrics['mse']/num_batches
+        reward = val_metrics['reward']/(num_batches * self.args.batchsize)
+ 
+        wandb.log({ 
+            "episode": episode, 
+            "val_scaled_pesq":pesq,
+            "val_pretrain_loss":loss,
+            "val_pesq":np.asarray(val_metrics["pesq"]).mean(),
+            "val_csig":np.asarray(val_metrics["csig"]).mean(),
+            "val_cbak":np.asarray(val_metrics["cbak"]).mean(),
+            "val_covl":np.asarray(val_metrics["covl"]).mean(),
+            "val_ssnr":np.asarray(val_metrics["ssnr"]).mean(),
+            "val_stoi":np.asarray(val_metrics["stoi"]).mean(),
+            "val_si-sdr":np.asarray(val_metrics["si-sdr"]).mean(),
+            "val_reward":reward
+        }) 
+        print(f"Episode:{episode} | VAL_PESQ:{np.asarray(val_metrics['pesq']).mean()} | VAL_LOSS:{loss} | REWARD: {reward}")
+        
+        self.actor.train()
+        if self.args.method == 'PPO':
+            self.actor.set_evaluation(False)
+            self.critic.train()
+        
+        return loss
+'''   
 
 
 parser = argparse.ArgumentParser()

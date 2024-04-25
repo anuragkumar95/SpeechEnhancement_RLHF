8ba7247affbe94f2bc55eb06774db7326002d3a6
Anurag Kumar <anuragkumar@Anurags-MacBook-Pro.local>
2024-04-25 11:14:17 -0400

    .

--- src/RLHF.py
+++ src/RLHF.py
@@ -573,7 +573,7 @@ class PPO:
                     
         return (step_clip_loss, step_val_loss, step_entropy_loss, step_pg_loss, step_sup_loss), \
                (target_values.mean(), VALUES.mean(), ep_kl_penalty, r_ts.mean(), ang_rewards.mean()), \
-               advantages.mean()  
+               mb_adv.mean()  
 
 
     def run_n_step_episode(self, batch, actor, critic, optimizer, n_epochs=3):

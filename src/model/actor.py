28d76fb45d351a73c9cec092f03e96126863553b
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-26 19:56:04 -0400

    .

--- src/model/actor.py
+++ src/model/actor.py
@@ -177,6 +177,7 @@ class MaskDecoder(nn.Module):
             
         elif self.dist is None:
             x_mu = self.final_conv(x).permute(0, 3, 2, 1).squeeze(-1)
+            print(f"x_mu:{x_mu.mean()}, x:{x.mean()}")
             x, x_logprob, x_entropy, params = self.sample(x_mu, None, action)
             print(f"eval:{self.evaluation}")
             if self.evaluation:

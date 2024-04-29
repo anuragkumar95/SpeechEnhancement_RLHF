39d0d0b415f49d9361f4b723c3f1db995265a662
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-29 14:12:39 -0400

    /

--- src/generate_audio_mixtures.py
+++ src/generate_audio_mixtures.py
@@ -254,11 +254,15 @@ def generate_ranking(mos_file, mixture_dir, save_dir, set='train'):
     FILES = [(file_name, mos_score) for file_name, mos_score in mos.iteritems()]
     FILES = sorted(FILES, key=lambda x:x[1], revers=True)
     PAIRS = itertools.combinations(FILES, 2)
+    print(f"TOTAL PAIRS:{len(PAIRS)}")
 
-    with open(os.path.join(save_dir, f'{set}.pairs'), 'w') as f:   
-        for (p1, m1), (p2, m2) in zip(PAIRS):
+    with open(os.path.join(save_dir, f'{set}.pairs'), 'w') as f:  
+        written = 0 
+        for (p1, m1), (p2, m2) in tqdm(zip(PAIRS)):
             if m1 - m2 > 0.25:
                 f.write(f"{p1} {p2}\n")
+                written += 1
+        print(f"SAVED PAIRS:{written}")
     
     '''
     with open(os.path.join(save_dir, f'{set}.ranks'), 'w') as f:    

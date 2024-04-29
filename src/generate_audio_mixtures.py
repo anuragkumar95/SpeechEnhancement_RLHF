9d83da9e779970f2c86895ea8c2cc396fb2cf1f1
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-29 14:42:46 -0400

    .

--- src/generate_audio_mixtures.py
+++ src/generate_audio_mixtures.py
@@ -260,7 +260,7 @@ def generate_ranking(mos_file, mixture_dir, save_dir, set='train'):
     n_size = 100000
     with open(os.path.join(save_dir, f'{set}.pairs'), 'w') as f:  
         #for (p1, m1) in tqdm(FILES):
-        for _  in range(n_size):
+        for _  in tqdm(range(n_size)):
             diff = 9999
             while(diff < 0.25):
                 k = np.random.choice(num_files, 2, replace=False)

c84ced2df8bcf04c296d8d958579c527a9b74737
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-28 22:53:30 -0400

    .

--- src/model/reward_model.py
+++ src/model/reward_model.py
@@ -22,19 +22,19 @@ class RewardModel(nn.Module):
         """
         x is (b * ch * f * t)
         """
-        x = x.permute(0, 3, 1, 2)
+        #x = x.permute(0, 3, 1, 2)
         x_pos = pos.permute(0, 3, 1, 2)
         x_neg = neg.permute(0, 3, 1, 2)
 
-        x_encoded = self.encoder.get_embedding(x)
+        #x_encoded = self.encoder.get_embedding(x)
         pos_encoded = self.encoder.get_embedding(x_pos)
         neg_encoded = self.encoder.get_embedding(x_neg)
         
-        pos_inp = torch.cat([x_encoded, pos_encoded], dim=1)
-        neg_inp = torch.cat([x_encoded, neg_encoded], dim=1)
+        #pos_inp = torch.cat([x_encoded, pos_encoded], dim=1)
+        #neg_inp = torch.cat([x_encoded, neg_encoded], dim=1)
 
-        pos_proj = self.reward_projection(pos_inp)
-        neg_proj = self.reward_projection(neg_inp)
+        pos_proj = self.reward_projection(pos_encoded)
+        neg_proj = self.reward_projection(neg_encoded)
 
         loss = -torch.log(F.sigmoid(pos_proj - neg_proj) + self.eps).mean()
    

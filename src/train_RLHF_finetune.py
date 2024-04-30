f9ce93282d206f26df1bf6cbf31b6d8183cdffc7
Anurag Kumar <anuragkumar@Anurags-MacBook-Pro.local>
2024-04-30 13:02:30 -0400

    .

--- src/train_RLHF_finetune.py
+++ src/train_RLHF_finetune.py
@@ -409,8 +409,6 @@ class Trainer:
             self.critic.train()
 
         loss, best_pesq = self.run_validation(0)
-        #loss = 99999 
-        #best_pesq = 0
         epochs_per_episode = self.args.ep_per_episode
         
         run_validation_step = 250 // (epochs_per_episode * self.args.episode_steps)

5fdd0f9e68fbce9461fb58db8f2a5e2da9566b32
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-04-30 02:10:06 -0400

    .

--- src/train_RLHF_finetune.py
+++ src/train_RLHF_finetune.py
@@ -454,7 +454,7 @@ class Trainer:
 
                         print(f"Epoch:{epoch} | Episode:{i+1} | Return: {batch_reward[0].item()} | Values: {batch_reward[1].item()}")
 
-                if i+1 % 1 == 0:
+                if i+1 % 100 == 0:
                 #Run validation after each episode
                 
                     loss, val_pesq = self.run_validation((epoch-1) * episode_per_epoch + (i+1))

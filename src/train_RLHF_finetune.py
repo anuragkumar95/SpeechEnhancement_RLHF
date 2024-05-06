39b8214e32705c7b1d6cc2b05127193b400201ee
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-05-06 16:14:47 -0400

    .

--- src/train_RLHF_finetune.py
+++ src/train_RLHF_finetune.py
@@ -263,7 +263,7 @@ class Trainer:
 
         #Forward pass through actor to get the action(mask)
         action, log_probs, _, _ = self.actor.get_action(inp)
-        sft_action, _, _ = self.trainer.init_model.get_action(inp)
+        
         if self.expert is not None:
             ref_log_probs, _ = self.trainer.init_model.get_action_prob(inp, action)
             ref_log_prob = ref_log_probs[0] + ref_log_probs[1][:, 0, :, :].permute(0, 2, 1) + ref_log_probs[1][:, 1, :, :].permute(0, 2, 1)

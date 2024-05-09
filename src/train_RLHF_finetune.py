245cfab011bc47d144168d7c428fa31aa925ae40
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-05-09 13:29:04 -0400

    .

--- src/train_RLHF_finetune.py
+++ src/train_RLHF_finetune.py
@@ -461,7 +461,7 @@ class Trainer:
                 #Run validation after each episode
                     loss, rm_score, val_pesq = self.run_validation((epoch-1) * episode_per_epoch + (i+1))
                     if val_pesq >= best_pesq or rm_score >= best_rm_score:
-                        best_pesq = max(best_pesq, val_pesq)
+                        best_pesq = val_pesq
                         best_rm_score = max(best_rm_score, rm_score)
                         self.trainer.init_model = copy.deepcopy(self.actor)
                         self.trainer.init_model = self.trainer.init_model.eval()

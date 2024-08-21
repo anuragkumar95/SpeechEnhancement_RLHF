2f2c2a1b20463cd1937ee5207d7901a5129db064
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-08-21 17:44:18 -0400

    .

--- src/model/MPSENet/actor.py
+++ src/model/MPSENet/actor.py
@@ -7,12 +7,41 @@ from model.MPSENet.conformer import ConformerBlock
 
 from torch.distributions import Normal
 
-from utils import get_padding_2d, LearnableSigmoid_2d
+#from utils import get_padding_2d, LearnableSigmoid_2d
 from pesq import pesq
 from joblib import Parallel, delayed
 
 
 #Taken from https://github.com/yxlu-0102/MP-SENet/blob/main/models/generator.py
+
+def get_padding(kernel_size, dilation=1):
+    return int((kernel_size*dilation - dilation)/2)
+
+
+def get_padding_2d(kernel_size, dilation=(1, 1)):
+    return (int((kernel_size[0]*dilation[0] - dilation[0])/2), int((kernel_size[1]*dilation[1] - dilation[1])/2))
+
+
+class LearnableSigmoid_1d(nn.Module):
+    def __init__(self, in_features, beta=1):
+        super().__init__()
+        self.beta = beta
+        self.slope = nn.Parameter(torch.ones(in_features))
+        self.slope.requiresGrad = True
+
+    def forward(self, x):
+        return self.beta * torch.sigmoid(self.slope * x)
+
+
+class LearnableSigmoid_2d(nn.Module):
+    def __init__(self, in_features, beta=1):
+        super().__init__()
+        self.beta = beta
+        self.slope = nn.Parameter(torch.ones(in_features, 1))
+        self.slope.requiresGrad = True
+
+    def forward(self, x):
+        return self.beta * torch.sigmoid(self.slope * x)
 
 class DenseBlock(nn.Module):
     def __init__(self, dense_channel=64, kernel_size=(3, 3), depth=4):

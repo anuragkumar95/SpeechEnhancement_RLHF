fc7d1e2b6508ee3aa2f3785b3bf515343d69e07c
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-08-21 16:36:06 -0400

    .

--- src/train_RLHF_finetune.py
+++ src/train_RLHF_finetune.py
@@ -149,8 +149,7 @@ class Trainer:
             self.expert = freeze_layers(self.expert, 'all')
             
             del cmgan_expert_checkpoint 
-            print(f"AAAAA")
-            print(f"Loaded checkpoint stored at {args.ckpt}. Resuming training...") 
+            #print(f"Loaded checkpoint stored at {args.ckpt}. Resuming training...") 
         
         self.reward_model = None
         if args.reward_pt is not None:

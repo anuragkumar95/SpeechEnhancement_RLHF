832b9c0771fa36da420ed6705901b8f237b044f1
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-08-28 14:50:07 -0400

    .

--- src/RLHF.py
+++ src/RLHF.py
@@ -629,7 +629,7 @@ class PPO:
                         if self.model == 'metricgan':
                             log_prob = log_probs
                             ref_log_prob = ref_log_probs
-                            old_log_prob = logprobs[mb_indx, ...].permute(0, 2, 1)
+                            old_log_prob = logprobs[mb_indx, ...]
                     
                     print(f"ref_logprob:{ref_log_prob.mean()}, {ref_log_prob.shape}")
                     print(f"new_logprob:{log_prob.mean(), log_prob.shape}")

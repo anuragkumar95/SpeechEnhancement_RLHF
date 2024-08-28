14b6f8c7775eff99585840837e786dbd195b5967
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-08-28 14:43:39 -0400

    .

--- src/speech_enh_env.py
+++ src/speech_enh_env.py
@@ -109,6 +109,8 @@ class SpeechEnhancementAgent:
             print(f"est_spec:{est_spec.shape}, mag:{mag.shape}, phase:{phase.shape}")
             est_audio = transform_spec_to_wav(mag, phase)
             est_mag = est_mag.unsqueeze(1)
+            est_real = est_real.unsqueeze(1)
+            est_imag = est_imag.unsquueze(1)
 
         next_state = {}
         next_state['noisy'] = est_spec

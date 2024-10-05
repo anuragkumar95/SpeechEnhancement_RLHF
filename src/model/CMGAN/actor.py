b89fd53e70b72579fe6ae7e30ff4617966836306
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-10-05 12:18:48 -0400

    .

--- src/model/CMGAN/actor.py
+++ src/model/CMGAN/actor.py
@@ -403,10 +403,11 @@ class TSCNet(nn.Module):
                 mag, complex_out = action
             #Add gaussian noise
             mask, r_logprob, _ = self.sample(mu=mask, x=mag)
-            r_logprob = r_logprob.squeeze(1)
-            
             complex_out, i_logprob, _ = self.sample(mu=complex_out, x=complex_out)
-            probs = r_logprob.permute(0, 2, 1) + i_logprob[:, 0, :, :] + i_logprob[:, 1, :, :]
+
+            print(r_logprob.shape, i_logprob.shape)
+
+            probs = r_logprob.squueze(1) + i_logprob[:, 0, :, :] + i_logprob[:, 1, :, :]
         
         
         out_mag = mask * mag

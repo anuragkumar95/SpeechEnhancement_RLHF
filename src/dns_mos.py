6381801c024c998f10d3fb64b4ce60fc21a58759
Anurag Kumar <anuragkumar@Anurags-MBP.lan>
2024-10-07 22:53:46 -0400

    .

--- src/dns_mos.py
+++ src/dns_mos.py
@@ -6,6 +6,7 @@ import argparse
 import concurrent.futures
 import glob
 import os
+import torch
 
 import librosa
 import numpy as np
@@ -48,26 +49,15 @@ class ComputeScore:
 
         return sig_poly, bak_poly, ovr_poly
 
-    def __call__(self, aud, sampling_rate, is_personalized_MOS):
-        #aud, input_fs = sf.read(fpath)
+    def __call__(self, aud, sampling_rate=16000):
         fs = sampling_rate
-        #if input_fs != fs:
-        #    audio = librosa.resample(aud, input_fs, fs)
-        #else:
         audio = aud
-        actual_audio_len = len(audio)
         len_samples = int(INPUT_LENGTH*fs)
         while len(audio) < len_samples:
             audio = np.append(audio, audio)
         
         num_hops = int(np.floor(len(audio)/fs) - INPUT_LENGTH)+1
         hop_len_samples = fs
-        #predicted_mos_sig_seg_raw = []
-        #predicted_mos_bak_seg_raw = []
-        #predicted_mos_ovr_seg_raw = []
-        #predicted_mos_sig_seg = []
-        #predicted_mos_bak_seg = []
-        #predicted_mos_ovr_seg = []
         predicted_p808_mos = []
 
         for idx in range(num_hops):
@@ -75,39 +65,21 @@ class ComputeScore:
             if len(audio_seg) < len_samples:
                 continue
 
-            #input_features = np.array(audio_seg).astype('float32')[np.newaxis,:]
             p808_input_features = np.array(self.audio_melspec(audio=audio_seg[:-160])).astype('float32')[np.newaxis, :, :]
-            #oi = {'input_1': input_features}
             p808_oi = {'input_1': p808_input_features}
             p808_mos = self.p808_onnx_sess.run(None, p808_oi)[0][0][0]
-            #mos_sig_raw,mos_bak_raw,mos_ovr_raw = self.onnx_sess.run(None, oi)[0][0]
-            #mos_sig,mos_bak,mos_ovr = self.get_polyfit_val(mos_sig_raw,mos_bak_raw,mos_ovr_raw,is_personalized_MOS)
-            #predicted_mos_sig_seg_raw.append(mos_sig_raw)
-            #predicted_mos_bak_seg_raw.append(mos_bak_raw)
-            #predicted_mos_ovr_seg_raw.append(mos_ovr_raw)
-            #predicted_mos_sig_seg.append(mos_sig)
-            #predicted_mos_bak_seg.append(mos_bak)
-            #predicted_mos_ovr_seg.append(mos_ovr)
             predicted_p808_mos.append(p808_mos)
 
-        #clip_dict = {'filename': fpath, 'len_in_sec': actual_audio_len/fs, 'sr':fs}
-        #clip_dict = {'filename': 'x', 'len_in_sec': actual_audio_len/fs, 'sr':fs}
-        #clip_dict['num_hops'] = num_hops
-        #clip_dict['OVRL_raw'] = np.mean(predicted_mos_ovr_seg_raw)
-        #clip_dict['SIG_raw'] = np.mean(predicted_mos_sig_seg_raw)
-        #clip_dict['BAK_raw'] = np.mean(predicted_mos_bak_seg_raw)
-        #clip_dict['OVRL'] = np.mean(predicted_mos_ovr_seg)
-        #clip_dict['SIG'] = np.mean(predicted_mos_sig_seg)
-        #clip_dict['BAK'] = np.mean(predicted_mos_bak_seg)
-        #clip_dict['P808_MOS'] = np.mean(predicted_p808_mos)
-        return np.mean(predicted_p808_mos)
+        mos = np.mean(predicted_p808_mos)
+        mos = torch.tensor(mos).reshape(-1, 1)
+        return mos
     
 
-    def get_scores(self, clips, is_personalized_eval, desired_fs=16000):
+    def get_scores(self, clips, desired_fs=16000):
         mos = []
         clips = [aud.detach().cpu().numpy() for aud in clips]
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_to_url = {executor.submit(self.__call__, clip, desired_fs, is_personalized_eval): clip for clip in clips}
+            future_to_url = {executor.submit(self.__call__, clip, desired_fs): clip for clip in clips}
             for future in tqdm(concurrent.futures.as_completed(future_to_url)):
                 clip = future_to_url[future]    
                 try:
